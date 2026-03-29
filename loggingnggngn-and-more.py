ffdaaa6db30c037decc7032183a91c03c253eaf4
YaroslavItStep <viktorholyak1@gmail.com>
2026-03-29 13:18:28 +0300

    more i guess

--- loggingnggngn-and-more.py
+++ loggingnggngn-and-more.py
@@ -1,17 +1,21 @@
 import logging
 
-# logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="w")
-# logging.debug("IM DEBUGGING")
-logging.basicConfig(
-    level=logging.INFO, 
-    filename="log.log", 
-    filemode="w", 
-    format="%(asctime)s: %(levelname)s - %(message)s"
-)
+# # logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="w")
+# # logging.debug("IM DEBUGGING")
 
-logging.info('The program was launched succesfully')
-# logging.debug('debug')
-# logging.info('info')
-# logging.warning('warning')
-# logging.error('error')
-# logging.critical('critical')
+# logging.basicConfig(
+#     level=logging.INFO, 
+#     filename="log.log", 
+#     filemode="w", 
+#     format="%(asctime)s: %(levelname)s - %(message)s"
+# )
+
+# logging.info('The program was launched succesfully')
+
+# # logging.debug('debug')
+# # logging.info('info')
+# # logging.warning('warning')
+# # logging.error('error')
+# # logging.critical('critical')
+
+# assert 2 + 2 == 3, "math isnt mathing"
