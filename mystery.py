a85d95f0f7ae908149fd7cdeac8c872a73a7b57e
YaroslavItStep <viktorholyak1@gmail.com>
2026-03-29 11:03:16 +0300

    MOREEEE

--- mystery.py
+++ mystery.py
@@ -1,9 +1,33 @@
-from email import iterators
+# my_list = [1, 2, 3, 4]
+# iterator = iter(my_list)
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
 
 
-my_list = [1, 2, 3, 4]
-iterator = iter(my_list)
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+class Counter:
+    def __init__(self, max_number):
+        self.i = 0
+        return self
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        self.i += 1
+        if self.i > self.max_number:
+            raise StopIteration
+        return self.i
+count = Counter(5)
+
+try:
+    print(count.__next__)
+    print(count.__iter__)
+
+    print(next(count))
+    print(iter(count))
+    print(next(count))
+except StopIteration:
+    print("Counter has reached her limit")
