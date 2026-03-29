edb154c54649118365e9b6a1126f62abfb4ba28a
YaroslavItStep <viktorholyak1@gmail.com>
2026-03-29 11:32:27 +0300

    idk probably the last

--- mystery.py
+++ mystery.py
@@ -33,16 +33,41 @@
 #     print("Counter has reached her limit")
 
 
-def raise_to_the_degrees(number, max_number):
-    i = 0
-    # while True:
-    #     yield number **i
-    #     i += 1
+# def raise_to_the_degrees(number, max_number):
+#     i = 0
+#     # while True:
+#     #     yield number **i
+#     #     i += 1
     
-    for _ in range(max_number):
-        yield number ** i
+#     for _ in range(max_number):
+#         yield number ** i
+#         i += 1
+# res = raise_to_the_degrees(10,10)
+# print(res)
+# for _ in res:
+#     print(_)
+
+
+# def helper(work):
+#     work_in_memory = work
+#     def helper(work):
+#         return f"i will help you with your {work_in_memory}. afterwards i will help you with {work}"
+#     return helper
+
+# helper = helper("homework")
+# print(helper("cleaning"))
+# print(helper("driving"))
+
+
+def generate_even_numbers(quantity):
+    even_numbers = []
+    i = 0
+
+    while (len(even_numbers) < quantity):
+        if i % 2 == 0:
+            even_numbers.append(i)
         i += 1
-res = raise_to_the_degrees(10,10)
-print(res)
-for _ in res:
-    print(_)
+    return even_numbers
+
+for i in generate_even_numbers(5):
+    print(i)
